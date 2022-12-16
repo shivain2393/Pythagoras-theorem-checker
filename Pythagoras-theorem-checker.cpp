7ffcad481d4ce096d4f0761150a481de93d27a1c
Shivain Sagar <shivain.sagar@gmail.com>
2022-12-16 14:05:00 +0530

    New version
    Improved the previous code

--- Pythagoras-theorem-checker.cpp
+++ Pythagoras-theorem-checker.cpp
@@ -16,19 +16,23 @@ cin>>ht;
 float sum;
 sum = b*b + ht*ht;
 
-cout<<endl<<"Checking for pythagoras theorem..."<<endl;
+cout<<endl<<"Checking for Pythagoras theorem..."<<endl;
 
 cout<<endl<<"Hypotenuse = "<<h<<endl<<"Base = "<<b<<endl<<"Height = "<<ht<<endl;
 
+cout<<endl<<"Since,"<<endl;
+
 if(h*h==sum){
 
-    cout<<endl<<h*h<<" = "<<b*b<<" + "<<ht*ht<<endl;
+    cout<<endl<<h*h<<" = "<<b*b<<" + "<<ht*ht;
+    cout<<endl<<h*h<<" = "<<sum<<endl;
 
     cout<<endl<<"The following values satisfy Pythagoras theorem.";
 }
 else{
 
-    cout<<endl<<h*h<<" =! "<<b*b<<" + "<<ht*ht<<endl;
+    cout<<endl<<h*h<<" != "<<b*b<<" + "<<ht*ht;
+    cout<<endl<<h*h<<" != "<<sum<<endl;
 
     cout<<endl<<"The following values  does not satisfy Pythagoras theorem.";
 }
